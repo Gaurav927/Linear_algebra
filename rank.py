f8b91497e6f7678d30a2b5d8c2b4da38829c4298
Gaurav Kumar <gauravkr927@gmail.com>
2020-07-11 10:30:08 +0530

    Add files via upload

---
+++ rank.py
@@ -0,0 +1,55 @@
+def swapRow(matrix, i, j):
+    temp = matrix[i]
+    matrix[i] = matrix[j]
+    matrix[j] = temp
+    return 
+
+def nonZero(a):
+    return a!=0
+
+def reduction(matrix, i_base , j):
+    multiplier = matrix[j][i_base]/matrix[i_base][i_base]
+
+    for i in range(len(matrix[0])):
+        matrix[j][i] = matrix[j][i] - multiplier*matrix[i_base][i]
+    return
+
+def rankOfMatrix(matrix):
+    row, col = len(matrix), len(matrix[0])
+
+    count = col
+
+    for i in range(row):
+        if nonZero(matrix[i][i]):
+            for j in range(col):
+                if j!=i:
+                    reduction(matrix, i, j)
+        else:
+            swap = False
+            for k in range(i+1, row):
+                if nonZero(matrix[k][i]):
+                    swapRow(matrix, k, i)
+                    swap = True
+                    break
+            if (not swap):
+                count -=1
+            i = i - 1
+    return count
+
+matrix = [[1,2,3,5],[1,2,3,5],[10,16,17,19],[10,23,17,19]]
+
+print("Rank of given matrix is {}".format(rankOfMatrix(matrix)))
+
+
+            
+
+
+
+
+
+
+
+
+
+
+
