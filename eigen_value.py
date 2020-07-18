522234661c1a9f254789af725652198ca25ddb69
Gaurav Kumar <gauravkr927@gmail.com>
2020-07-18 17:15:20 +0530

    Add files via upload

---
+++ eigen_value.py
@@ -0,0 +1,17 @@
+import gram_schmidt
+import numpy as np
+from numpy import linalg as LA
+
+def iterate(A):
+    A = np.array(A)
+    for _ in range(20):
+        Q = gram_schmidt.get_orthogonal(A)
+        A = Q.T@A@Q
+        
+    return [A[i][i] for i in range(A.shape[0])]
+
+matrix = [[1,2,10],[0,0,5],[0,3,6]]
+print(iterate(matrix))
+
+    
+
