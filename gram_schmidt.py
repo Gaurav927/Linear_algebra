cee2b82080f67299589dbb1770541f61bb1fac22
Gaurav Kumar <gauravkr927@gmail.com>
2020-07-18 17:18:37 +0530

    Update gram_schmidt.py

--- gram_schmidt.py
+++ gram_schmidt.py
@@ -9,16 +9,17 @@ def get_orthogonal(A):
     A = np.array(A)
     A = np.transpose(A)
     row = len(A)
-    matrix = np.zeros_like(A)
-    matrix[0] = normalize(A[0])
-
+    matrix = []
+    matrix.append(normalize(A[0]))
 
     for i in range(1,row):
         temp = A[i]
         for j in range(i):
             temp = temp - np.dot(matrix[j],A[i])*matrix[j]
-        matrix[i] = normalize(temp)
-    return matrix
+        matrix.append(normalize(temp))
+        
+    matrix = np.array(matrix)
+    return np.transpose(matrix)
 
-A =np.array([[1,2,10],[0,0,5],[0,3,6]])
+A =np.array([[1,2,4],[1,0,5],[0,3,6]])
 print("Orthogonal basis of the above matrix \n{}".format(get_orthogonal(A)))
