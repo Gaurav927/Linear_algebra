9f06d9ad8dac5e2d15906efb31bacc1fdced4629
Gaurav Kumar <gauravkr927@gmail.com>
2020-07-19 08:13:30 +0530

    Update eigen_value.py

--- eigen_value.py
+++ eigen_value.py
@@ -1,18 +1,28 @@
+from determinant_row_reduction import determinant
 import gram_schmidt
 import numpy as np
-from numpy import linalg as LA
+import determinant_row_reduction
 
-# use gram_schmidt function & the concept of similar matrix to calculate eigen values
+EPSILON = 1e-4
+
 def iterate(A):
     A = np.array(A)
+    matrix = A.copy()
+
     for _ in range(20):
         Q = gram_schmidt.get_orthogonal(A)
         A = Q.T@A@Q
-        
-    return [A[i][i] for i in range(A.shape[0])]
-
-matrix = [[1,2,10],[0,0,5],[0,3,6]]
-print(iterate(matrix))
-
     
+    pos_sol = [A[i][i] for i in range(A.shape[0])]
+    sol = []
+    for val in pos_sol:
+        temp = matrix -val*np.identity(len(matrix))
+        if abs(determinant_row_reduction.determinant(temp))<= EPSILON:
+            sol.append(val)
 
+    return sol
+
+matrix = [[1,2,10],[1,4,5],[0,3,6]]
+
+values = iterate(matrix)
+print("Real eigen values of the above matrix is\n{}".format(values))
